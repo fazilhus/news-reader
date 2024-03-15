964552b02ac801d4b93f888608987d8ee5e77c77
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2024-03-15 09:35:55 +0100

    api overhaul, now supports async fetch

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,30 @@ use newsapi::{*};
 
 mod theme;
 
-fn render_articles(articles: &Articles) {
+fn render_articles(articles: &Vec<Article>) {
     let theme = theme::default();
     theme.print_text("# Top Headlines!\n\n");
-    for article in &articles.articles {
-        theme.print_text(&format!("'{}'", article.title));
-        theme.print_text(&format!("> *{}*", article.url));
+    for article in articles {
+        theme.print_text(&format!("'{}'", article.title()));
+        theme.print_text(&format!("> *{}*", article.url()));
         theme.print_text("---");
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let _ = dotenv();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let _ = dotenv().ok();
 
     let api_key = std::env::var("API_KEY")?;
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
-    let url = format!("{}{}", url, api_key);
-    let articles = newsapi::get_articles(&url)?;
-    render_articles(&articles);
+    
+    let binding = NewsAPI::new(&api_key);
+    let mut newsapi = binding;
+    newsapi
+        .endpoint(EndPoint::TopHeadlines)
+        .country(Country::Us); 
+
+    let newsapi_response = newsapi.fetch_async().await?;
+    //let newsapi_response = newsapi.fetch()?;
+    render_articles(newsapi_response.articles());
     Ok(())
 }
